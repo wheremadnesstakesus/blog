09e816cf469b957183949deb4b619865e6642b26
José Manuel Rosa <josemanuel.rosamoncayo+github@gmail.com>
2020-05-10 18:41:44 +0100

    feat: add analytics plugin

--- src/components/SEO/SEO.js
+++ src/components/SEO/SEO.js
@@ -26,18 +26,6 @@ function SEO({ description, keywords, image, isPost, title, url }) {
         <meta name="twitter:title" content={title} />
         <meta name="twitter:description" content={description} />
         <meta name="twitter:image" content={image} />
-
-        <script defer type="text/javascript">
-          {`(function(p, a, n, d, o, b) {
-            o = n.createElement('script'); o.type = 'text/javascript'; o.async = true; o.src = 'https://tag.rightmessage.com/'+p+'.js';
-            b = n.getElementsByTagName('script')[0]; b.parentNode.insertBefore(o, b);
-            d = function(h, u, i) { var o = n.createElement('style'); o.id = 'rmcloak'+i; o.type = 'text/css';
-                o.appendChild(n.createTextNode('.rmcloak'+h+'{visibility:hidden}.rmcloak'+u+'{display:none}'));
-                b.parentNode.insertBefore(o, b); return o; }; o = d('', '-hidden', ''); d('-stay-invisible', '-stay-hidden', '-stay');
-            setTimeout(function() { o.parentNode && o.parentNode.removeChild(o); }, a);
-        })('UA-ANALITYCS', 2500, document);
-      `}
-        </script>
       </Helmet>
     </React.Fragment>
   )
