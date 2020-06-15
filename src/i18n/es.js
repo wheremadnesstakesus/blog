1152a26736c35e2163439f6c351671dbc1851ac3
José Manuel Rosa <josemanuel.rosamoncayo+github@gmail.com>
2020-06-15 18:00:08 +0100

    feat: por europa y mas all

--- src/i18n/es.js
+++ src/i18n/es.js
@@ -5,9 +5,9 @@ module.exports = {
   description: 'Blog de viajes donde dos locos contaran sus pequeñas aventuras alrededor del mundo',
   keywords: ['viajes', 'vanlife', 'aventuras'],
   heading: (days) =>
-    `Somos Federica y José y llevamos <b>{${daysBetween(
+    `Somos Federica y José y llevamos <b>${daysBetween(
       days,
-    )}} días viviendo y viajando en nuestra furgoneta</b> , a la que llamamos Flowerneta y aquí te iremos contando todas nuestras aventuras, pensamientos y demás locuras que se nos vayan ocurriendo. Te unes a nuestra locura!`,
+    )} días viviendo y viajando en nuestra furgoneta</b> , a la que llamamos Flowerneta y aquí te iremos contando todas nuestras aventuras, pensamientos y demás locuras que se nos vayan ocurriendo. Te unes a nuestra locura!`,
   latestsPosts: 'Últimas publicaciones!',
   builtWith: (author) => `Hecho por ${author} con muchísimo `,
   copyright: 'Todos los derechos reservados',
