053b4ca0934bb6722030dfe15a5aad2df4c80aee
José Manuel Rosa <josemanuel.rosamoncayo+github@gmail.com>
2022-02-01 12:22:21 +0100

    feat: ass the basics with nextjs and tailwind

---
+++ next.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  compress: false,
+  distDir: '.build',
+  webpack5: true,
+  pageExtensions: ['js'],
+  poweredByHeader: false,
+  reactStrictMode: true,
+  env: {
+    isDevelopment: ['development', 'test'].includes(process.env.NODE_ENV)
+  },
+  webpack (config, options) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: /\.js?$/,
+      include: [options.dir],
+      use: [
+        'next-swc-loader',
+        {
+          loader: '@svgr/webpack',
+          options: { babel: false }
+        }
+      ]
+    })
+
+    return config
+  }
+}
